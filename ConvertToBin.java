ed2aba9471f9fa7c9f7baf338be1da91a6112381
Aakash Madan <madan.aakash2@gmail.com>
2018-10-07 14:43:38 +0530

    Update ConvertToBin.java

--- ConvertToBin.java
+++ ConvertToBin.java
@@ -1,26 +1,19 @@
 public class ConvertToBin {
-		public int[] convertBinary(int num){
-			int a[]=new int[11];
-			int b[];
-			int i=0;
-			do{
+	public int[] convertBinary(int num){
+		int a[]=new int[11];
+		int b[];
+		int i=0;
+		while(num!=0){
 			int r=num%2;
 			num=num/2;
 			a[i]=r;
-			i++;
-			}while(num!=0);
-			//for(int j=0;j<a.length;j++){
-				b = new int[a.length];
-
-				for(int k = 0 ; k<a.length; k++){
-				b[k] = a[a.length -1 -k];
-
-
-				}
-				//for(i=0;i<b.length;i++)
-				//System.out.print(b[i]);
-
-			return b;
+			i++;	
+		}	
+		b = new int[a.length];
+		for(int k = 0 ; k<a.length; k++){
+			b[k] = a[a.length -1 -k];
 		}
+		return b;
+	}
 }
 		
